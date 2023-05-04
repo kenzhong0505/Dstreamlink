aca1725ec93654ffdac9f34411e71c1b2b759178
kenzhong0505 <89652434+kenzhong0505@users.noreply.github.com>
2023-05-04 09:19:50 +0800

    Update dash.py

--- src/streamlink/stream/dash.py
+++ src/streamlink/stream/dash.py
@@ -289,8 +289,6 @@ class DASHStream(Stream):
         # Search for suitable video and audio representations
         for aset in mpd.periods[0].adaptationSets:
             for rep in aset.representations:
-                if rep.contentProtections:
-                    raise PluginError(f"{source} is protected by DRM")
                 if rep.mimeType.startswith("video"):
                     video.append(rep)
                 elif rep.mimeType.startswith("audio"):  # pragma: no branch
